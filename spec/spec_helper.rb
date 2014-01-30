0098920fa06ecba3589b409cc1e6133a4c3f6430
Brad Chase <brad@webappfromscratch.com>
2014-01-30 10:23:41 +0000

    fix error with :focus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
 
-  config.filter_run :focus
+  # config.filter_run :focus
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
 end
