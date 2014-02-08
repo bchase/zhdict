03eda54c4ebdc035b4e707614ad168303ce1a779
Brad Chase <bosco87@gmail.com>
2014-02-08 14:42:33 -0500

    specd Word#build_from_cedict_line

--- spec/zhdict/word/build_class_methods_spec.rb
+++ spec/zhdict/word/build_class_methods_spec.rb
@@ -4,14 +4,21 @@ require 'test_models'
 describe Zhdict::Word::BuildClassMethods do
   describe '.build_from_cedict_line' do
     let(:cedict_line) {
-      "做 做 [zuo4] /to do/to make/to produce/to write/to compose/to act as/to engage in/to hold (a party)/to be/to become/to function (in some capacity)/to serve as/to be used for/to form (a bond or relationship)/to pretend/to feign/to act a part/to put on appearance/"
+      '還 还 [hai2] /still/still in progress/still more/yet/even more/in addition/fairly/passably (good)/as early as/even/also/else/'
     }
     let(:word) { Word.build_from_cedict_line(cedict_line) }
 
-    subject { word.traditional_characters }
-    it { should eq('做') }
+    # subject { word.traditional_characters }
+    # it { should eq('還') }
 
-    # it 'adds the right attribute values from the line' do
-    # end
+    # subject { word }
+    it 'properly parses the CEDict line' do
+      word.traditional_characters.should eq('還')
+      word.simplified_characters.should eq('还')
+      word.pronunciation.should eq('hai2')
+      word.glosses.split("\n").first.should eq('still')
+
+      word.raw_entry.should eq(cedict_line)
+    end
   end
 end
