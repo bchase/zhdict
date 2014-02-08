eb791283d14de2fd3787d99e4a4f1c9999847460
Brad Chase <bosco87@gmail.com>
2014-02-08 17:09:01 -0500

    note

--- scratch/api.rb
+++ scratch/api.rb
@@ -10,8 +10,6 @@ class ZhdictCreateWords < ActiveRecord::Migration
   def change
     create_table(:words) do |t|
       t.string :traditional_characters
-      t.string :simplified_characters
-      t.string :pronunciation
       t.text   :glosses
 
       t.string :raw_entry
